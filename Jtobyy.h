056440de5f6f8ad160b0e95ea48ce1caccfa9671
Jtobyy <jtobi8161@gmail.com>
2021-08-23 02:37:40 +0000

    implement simple shell version 0

--- Jtobyy.h
+++ Jtobyy.h
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <wait.h>
 
 char **split(char *str, char *del);
 
--- simpleshellv1.c
+++ simpleshellv1.c
@@ -1,45 +1,52 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <wait.h>
+#include "Jtobyy.h"
 
 /**
  *main - a first version of a super simple shell
  *that can run commands with their full path, without any argument.
+ *@argc: number of command line arguments
+ *@argv: array of command line arguments
  *Return: Always 0
  */
-int main(void)
+int main(int argc, char **argv)
 {
 int i;
 char *lineptr;
-char *argv[2];
+char *args[2];
 size_t n;
 int w;
-printf("#Jtoby's simple shell v1#\n");
+if (argc != 1)
+{
+write(STDERR_FILENO, "usage: ./hsh\n", 13);
+exit(100);
+}
 for (i = 0; ; i++)
 {
-printf("#sshell$ ");
+write(STDERR_FILENO, "$ ", 2);
 lineptr = NULL;
 n = getline(&lineptr, &n, stdin);
-argv[0] = lineptr;
-argv[1] = NULL;
-argv[0][n - 1] = '\0';
+if (n != (size_t)-1)
+args[0] = lineptr;
+else
+break;
+args[1] = NULL;
+args[0][n - 1] = '\0';
 if (fork() == 0)
 {
-if (execve(argv[0], argv, NULL) == -1)
+if (execve(args[0], args, NULL) == -1)
 {
-perror("Error");
+free(lineptr);
+perror(argv[0]);
 exit(2);
 }
 }
 else
 {
 wait(&w);
+free(lineptr);
 continue;
 }
 }
+free(lineptr);
 return (0);
 }
 
