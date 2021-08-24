a3ab2a2e292f0d89683cd5e7a44909a03a6789cc
Jtobyy <jtobi8161@gmail.com>
2021-08-24 14:49:16 +0000

    Simple shell version 02 handles command line arguments

--- simpleshellv01.c
+++ simpleshellv01.c
@@ -10,43 +10,105 @@
 int main(int argc, char **argv)
 {
 int i;
-char *lineptr;
-char *args[2];
-size_t n;
 int w;
+size_t n;
+char *lineptr;
+char **args;
+arg_list *head;
 if (argc != 1)
 {
 write(STDERR_FILENO, "usage: ./hsh\n", 13);
-exit(100);
+exit(97);
 }
 for (i = 0; ; i++)
 {
-write(STDERR_FILENO, "$ ", 2);
+write(STDOUT_FILENO, "$ ", 2);
 lineptr = NULL;
 n = getline(&lineptr, &n, stdin);
-if (n != (size_t)-1)
-args[0] = lineptr;
-else
+if (n == (size_t)-1)
+{
+free(lineptr);
 break;
-args[1] = NULL;
-args[0][n - 1] = '\0';
+}
+head = linktoken(lineptr, " \n");
+args = linktolist(head);
 if (fork() == 0)
 {
 if (execve(args[0], args, NULL) == -1)
 {
 free(lineptr);
 perror(argv[0]);
-exit(2);
+exit(99);
 }
 }
 else
 {
 wait(&w);
 free(lineptr);
-continue;
 }
 }
 free(lineptr);
 return (0);
 }
 
+/**
+ *linktoken - links the strings got from strtok
+ *using a singly linked list
+ *@str: initial string
+ *@del: delimeter
+ *Return: pointer to the head of the list
+ */
+arg_list *linktoken(char *str, char *del)
+{
+int j;
+char *token;
+arg_list *head;
+arg_list *el;
+head = NULL;
+el = NULL;
+for (j = 0; ; j++, str = NULL)
+{
+token = strtok(str, del);
+if (token == NULL)
+break;
+el = malloc(sizeof(*el) * 1);
+el->token = token;
+if (j == 0)
+{
+el->next = NULL;
+head = el;
+}
+else
+{
+el->next = head;
+head = el;
+}
+}
+return (head);
+}
+
+/**
+ *linktolist - stores the token elements of
+ *a singly linked list in an array of char*
+ *with a null pointer as the last element of the array
+ *@head: pointer to head of linked list
+ *Return: array of char * containing values of
+ *elements of the linked list
+ */
+char **linktolist(arg_list *head)
+{
+int i;
+arg_list *tmp;
+char **buf;
+tmp = head;
+for (i = 0; tmp != NULL; i++, tmp = tmp->next)
+continue;
+buf = malloc(sizeof(*buf) * (i + 1));
+if (buf == NULL)
+exit(98);
+tmp = head;
+buf[i] = NULL;
+for (i = i - 1; i >= 0; i--, tmp = tmp->next)
+buf[i] = strdup(tmp->token);
+return (buf);
+}
