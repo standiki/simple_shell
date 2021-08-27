41b1fbcb0c422ed17f0a6925f7fbfb6bb7908559
stanflows <atakistanley@gmail.com>
2021-08-28 01:25:18 +0300

    Revert "Hot fix"
    This reverts commit ef41de7bc96a7a2199f7a443aa48114b378be794.

--- prompt.c
+++ prompt.c
@@ -1,15 +1,18 @@
 #include "shell.h"
+#include "Jtobyy.h"
 
 /**
-* prompt_user - prints $ to let user know the program is
-* ready to take their input
-* prints the prompt if the shell is in interactive mode
-* Return: no return
-*/
-void prompt_user(void)
+ * prompt - Function that print shell prompt
+ *
+ * Return: void
+ */
+void prompt(void)
 {
-	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
-		flags.interactive = 1;
-	if (flags.interactive)
-		write(STDERR_FILENO, "$ ", 2);
+char name[] = "#cisfun$ ";
+int i = 0;
+while (name[i])
+{
+_putchar(name[i]);
+i++;
+}
 }
